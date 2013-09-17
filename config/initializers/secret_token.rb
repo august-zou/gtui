e2ad0b31185c9bb4999e72b50f64c888f693a864
august-zou <zjzouxu@gmail.com>
2013-09-17 13:56:28 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gtui::Application.config.secret_key_base = 'fee040beb88eb97c607433a81eda6506edd6ff050171deeb63e60334fddf55dec897a58062074ef41cec84ac6235e18482e0f30d116f55a1757738dfcf9c855a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gtui::Application.config.session_store :cookie_store, key: '_gtui_session'
